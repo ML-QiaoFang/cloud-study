44b30c1f7b1ab56f0bcbc608bc94e2d20e70f657
zfang <2686641845@qq.com>
2023-05-30 20:51:52 +0800

    GateWay9527配置路由1

---
+++ cloud-gateway-gateway9527/src/main/java/com/qfang/springcloud/GateWayMain9527.java
@@ -0,0 +1,18 @@
+package com.qfang.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author qfang
+ * @date 2023/5/30 - 20:07
+ */
+
+@EnableEurekaClient
+@SpringBootApplication
+public class GateWayMain9527 {
+    public static void main(String[] args) {
+        SpringApplication.run(GateWayMain9527.class, args);
+    }
+}
